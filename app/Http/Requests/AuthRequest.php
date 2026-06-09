59ffcdd615e9322ac82c8a25b02446ce3d15a3bf
Roman A. Aguero Hernandez <romandecuba@gmail.com>
2026-06-09 16:00:04 -0400

    Auth controller for login/logout a user.

---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.unique' => __('Este correo electrónico ya está en uso.')
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,50 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
+use App\Http\Controllers\AuthorityController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\ProceedingController;
+use App\Http\Controllers\OpinionController;
+use App\Http\Controllers\InterventionController;
+use App\Http\Controllers\PlanMaestroApiController;
+use App\Http\Controllers\UserController;
 
-Route::middleware('auth:sanctum')->group(function () {
-	Route::get('/user', function (Request $request) {
-    		return $request->user();
-	});
+use App\Http\Controllers\Auth\AuthController;
+
+Route::prefix('v1/plan-maestro')->group(function () {
+    Route::get('/', [PlanMaestroApiController::class, 'index']);
+    Route::get('/entities', [PlanMaestroApiController::class, 'getEntities']);
+    Route::get('/inscriptions', [PlanMaestroApiController::class, 'getInscriptions']);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+	Route::apiResource('authorities', AuthorityController::class)->missing(function (Request $request) {
+        return Redirect::route('authorities.index');
+    });
+
+	Route::apiResource('people', PersonController::class)->missing(function (Request $request) {
+        return Redirect::route('people.index');
+    });
+
+	Route::apiResource('proceedings', ProceedingController::class)->missing(function (Request $request) {
+        return Redirect::route('proceedings.index');
+    });
+
+	Route::apiResource('opinions', OpinionController::class)->missing(function (Request $request) {
+        return Redirect::route('opinions.index');
+    });
+
+	Route::apiResource('interventions', InterventionController::class)->missing(function (Request $request) {
+        return Redirect::route('interventions.index');
+    });
+
+	Route::apiResource('users', UserController::class)->missing(function (Request $request) {
+        return Redirect::route('users.index');
+    });
 });
