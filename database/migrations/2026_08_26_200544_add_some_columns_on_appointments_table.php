dd3e224d3ba31f251b27050c2ffd682860cf7afb
Roman A. Aguero Hernandez <romandecuba@gmail.com>
2026-08-26 21:21:12 +0000

    Face2FaceCommissionRequest migration and notification class renames to respond to the new Appoinment model. Face2FaceCommissionRequest table remove migration added.

---
+++ database/migrations/2026_08_26_200544_add_some_columns_on_appointments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->foreignId('signed_to')->constrained('users')->onDelete('cascade');
+            $table->foreignId('attended_by')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
